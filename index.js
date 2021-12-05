f4dd9239aee9601e957f4524e7b544d8d7df82bf
MajesticeM <67010913+MajesticeM@users.noreply.github.com>
2021-12-05 18:54:02 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+
+//Employee Inheritance
+function Employee(title) 
+{
+    function setTitle(titlePlaceholder)
+    {
+        titlePlaceholder=title;
+        return titlePlaceholder;
+    };
+    
+    function getTitle()
+    {
+        return title;
+    }
+}
+
+function Engineer(title, isManager) 
+{
+    function setIsManager(managerPlaceholder)
+    {
+        managerPlaceholder=isManager;
+        return managerPlaceholder;
+    };
+    
+    function getIsManager()
+    {
+        //Incomplete
+        const one=Engineer();
+        return 
+    }
+}
